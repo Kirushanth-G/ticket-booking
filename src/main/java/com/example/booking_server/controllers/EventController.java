c0a8f7c329c8003915637c9182b50ba851a01f40
Kirus <kirushanthh13@gmail.com>
2026-01-14 08:37:36 +0530

    event creation adjusted

--- src/main/java/com/example/booking_server/controllers/EventController.java
+++ src/main/java/com/example/booking_server/controllers/EventController.java
@@ -1,8 +1,8 @@
 package com.example.booking_server.controllers;
 
+import com.example.booking_server.dtos.EventDto;
 import com.example.booking_server.dtos.EventRequest;
-import com.example.booking_server.mappers.EventMapper;
-import com.example.booking_server.repositories.EventRepository;
+import com.example.booking_server.services.EventService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,26 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Map;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/events")
 public class EventController {
-    private final EventRepository eventRepository;
-    private final EventMapper eventMapper;
+    private EventService eventService;
 
     @PostMapping
     public ResponseEntity<?> createEvent(
             @Valid @RequestBody EventRequest request,
             UriComponentsBuilder uriBuilder){
-        if(eventRepository.existsByEventNameAndEventDate(request.getEventName(), request.getEventDate())){
-            return ResponseEntity.badRequest().body(Map.of("event", "An event with the same name and date already exists."));
-        }
-        var event = eventMapper.toEntity(request);
-        eventRepository.save(event);
-
-        var dto = eventMapper.toDto(event);
+        EventDto dto = eventService.createEvent(request);
         var uri = uriBuilder.path("/events/{id}").buildAndExpand(dto.getEventId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
--- src/main/java/com/example/booking_server/services/EventService.java
+++ src/main/java/com/example/booking_server/services/EventService.java
@@ -1,4 +1,27 @@
 package com.example.booking_server.services;
 
+import com.example.booking_server.dtos.EventDto;
+import com.example.booking_server.dtos.EventRequest;
+import com.example.booking_server.entities.Event;
+import com.example.booking_server.exceptions.EventAlreadyExistsException;
+import com.example.booking_server.mappers.EventMapper;
+import com.example.booking_server.repositories.EventRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class EventService {
+    //constructor injection
+    private final EventRepository eventRepository;
+    private final EventMapper eventMapper;
+
+    public EventDto createEvent(EventRequest request){
+        if(eventRepository.existsByEventNameAndEventDate(request.getEventName(), request.getEventDate())){
+            throw new EventAlreadyExistsException("An event with the same name and date already exists.");
+        }
+        Event event = eventMapper.toEntity(request);
+        Event savedEvent = eventRepository.save(event);
+        return eventMapper.toDto(savedEvent);
+    }
 }
